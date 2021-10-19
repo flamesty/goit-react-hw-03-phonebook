05d66f864e2f356e9213f819b336735b871b0436
Vladyslav Sukachev <79707548+flamesty@users.noreply.github.com>
2021-10-19 10:08:35 +0300

    hw-fix-04

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -31,6 +31,8 @@ class ContactForm extends Component {
               className={s.input}
               type="text"
               name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
               value={name}
               onChange={this.handleChange}
               placeholder="First Name Last Name"
@@ -41,8 +43,10 @@ class ContactForm extends Component {
             Number
             <input
               className={s.input}
-              type="number"
+              type="tel"
               name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
               value={number}
               onChange={this.handleChange}
               placeholder="123456789"
